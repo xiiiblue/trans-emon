c7d60496c63c54f38abe9271525b95d1c21b2f0e
BlueXIII <bluexiii@163.com>
2022-01-12 13:11:09 +0800

    新增Excel相似度比对

--- excel_similarity.py
+++ excel_similarity.py
@@ -1,9 +1,11 @@
+import jieba
 import xlrd
 from xlutils.copy import copy
 
 TITLE_ROW = 1
 SHEET_IDX = 0
 FILE_SUFFIX = '(含相似度建议)'
+THRESHOLD = 0.9
 
 
 def get_dest_path(src):
@@ -20,6 +22,40 @@ def get_title_col_id(title_name, titles):
     for col_id, title in enumerate(titles):
         if title_name == title:
             return col_id
+
+
+def is_symbol(char):
+    """
+    判断是否为特殊字符
+    """
+    symbols = "~!@#$%^&*()_+-*/<>,.[]\/（）【】，。《》"
+    for symbol in symbols:
+        if char == symbol:
+            return True
+    return False
+
+
+def get_oc_similarity(list_a, list_b):
+    """
+    判断相似度(Overlap Coefficient算法)
+    """
+    set_a = set(list_a)
+    set_b = set(list_b)
+    set_c = set_a.intersection(set_b)
+    return float(len(set_c)) / float(min(len(set_a), len(set_b)))
+
+
+def format_similarity_list(similarity_list):
+    """
+    格式化相似内容列表
+    """
+    text = ''
+    for row in similarity_list:
+        ratio, dest_row_id, dest_sentence = row
+        if text:
+            text += '  |  '
+        text += f'{dest_sentence}  相似度:{ratio}  行号:{dest_row_id}'
+    return text
 
 
 def similarity(path, src_title, dest_title):
@@ -45,8 +81,13 @@ def similarity(path, src_title, dest_title):
     src_col_id = get_title_col_id(src_title, titles)
     dest_col_id = get_title_col_id(dest_title, titles)
 
-    # 遍历Sheet页
-    sentence_list = []
+    print(f'原始标题: {src_title}  原始列ID: {src_col_id}  推荐标题: {dest_title}  推荐列ID: {dest_col_id}')
+
+    if not src_col_id or not dest_col_id:
+        raise RuntimeError('标题名称不存在')
+
+    # 遍历Sheet页取原始列
+    sentence_dict = {}
     nrows = read_sheet.nrows
     for row_id in range(nrows):
         # 标题及之上的行不处理
@@ -56,11 +97,55 @@ def similarity(path, src_title, dest_title):
         # 读取行
         row = read_sheet.row_values(row_id)
 
-        # 加入列表
+        # 读取单元格
         cell_value = row[src_col_id]
-        sentence_list.add(cell_value)
 
-    print(sentence_list)
+        # 分词
+        seg_list = jieba.lcut(cell_value.strip())
+
+        # 剔除特殊字符
+        for seg in seg_list:
+            if is_symbol(seg):
+                seg_list.remove(seg)
+
+        # 加入字典
+        cell_data = (cell_value, seg_list)
+        sentence_dict[row_id] = cell_data
+
+    # 比较相似度
+    similarity_dict = {}
+    for src_row_id, src_cell_data in sentence_dict.items():
+        src_sentenct, src_seg_list = src_cell_data
+        similarity_list = []
+        for dest_row_id, dest_cell_data in sentence_dict.items():
+            dest_sentenct, dest_seg_list = dest_cell_data
+            if src_row_id == dest_row_id:
+                continue
+            ratio = get_oc_similarity(src_seg_list, dest_seg_list)
+            if ratio > THRESHOLD:
+                similarity_list.append((ratio, dest_row_id, dest_sentenct))
+
+        if similarity_list:
+            similarity_dict[src_row_id] = similarity_list
+
+    print(similarity_dict)
+
+    # 改写Excel
+    for row_id in range(nrows):
+        if row_id <= TITLE_ROW:
+            continue
+
+        # 读取行
+        row = read_sheet.row_values(row_id)
+
+        # 读取单元格
+        cell_value = row[src_col_id]
+
+        if row_id in similarity_dict:
+            similarity_list = similarity_dict[row_id]
+            similarity_text = format_similarity_list(similarity_list)
+            print(f'行号: {row_id}  内容: {cell_value}  推荐: {similarity_text} \n')
+            write_sheet.write(row_id, dest_col_id, similarity_text)
 
     # 保存Excel
     dest_path = get_dest_path(path)
@@ -70,6 +155,6 @@ def similarity(path, src_title, dest_title):
 
 if __name__ == '__main__':
     path = '/Users/bluexiii/Downloads/export/客户管理20220110103625.xls'
-    src_title = '*项目名称'
-    dest_title = '相似项目名称'
+    src_title = '*客户名称'
+    dest_title = '相似客户名称推荐'
     similarity(path, src_title, dest_title)
