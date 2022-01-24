1933f138e1bda0c7f1df9e5f76bb6d80e6a0447c
BlueXIII <bluexiii@163.com>
2022-01-24 13:29:28 +0800

    更新测试用例

--- tests/playground.py
+++ tests/playground.py
@@ -1,4 +1,4 @@
-import uuid
-
-s = str(uuid.uuid4()).upper()
-print(s)
+url = 'http://http-dyn.abuyun.com:9020'
+url_split = url.split('://')
+url = f'{url_split[0]}://aaa:bbb@{url_split[1]}'
+print(url)
